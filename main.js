03f177e02235acb120cb979f7a8a2882947a7bdf
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-09 00:08:10 +0200

    added operation function

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let firstNum
+let secondNum
+let operator
+
+
 //Adding function
 const getSum = (a, b) => a + b
 
@@ -9,3 +14,19 @@ const getProduct = (a, b) => a * b
 
 //Adding function
 const getDivision = (a, b) => a / b
+
+//Operation function
+const operation = (a, b, c) => {
+    if(c === '+'){
+        return getSum(a, b)
+    }
+    else if(c === '-'){
+        return getDifference(a, b)
+    }
+    else if(c === '*'){
+        return getProduct(a, b)
+    }
+    else if(c === '/'){
+        return getDivision(a, b)
+    }
+}

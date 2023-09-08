76ddbeabcb701c3f221da2ff6abf52c647c33a09
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-08 23:50:20 +0200

    added math functions

---
+++ main.js
@@ -0,0 +1,19 @@
+//Adding function
+function getSum(a, b){
+    return a + b
+}
+
+//Subtracting function
+function getDifference(a, b){
+    return a - b
+}
+
+//Multiplication function
+function getProduct(a, b){
+    return a * b
+}
+
+//Adding function
+function getDivision(a, b){
+    return a / b
+}
